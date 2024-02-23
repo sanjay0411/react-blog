cdf183e7fec73edead170296ac96a77eef114fb1
Sanjay <sanjaykumarbe575@gmail.com>
2024-02-23 16:45:10 +0530

    added BrowserRouter.Routes,Router to 5 components

--- My-app/src/App.js
+++ My-app/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
 import AboutPage from "./pages/AboutPage";
 import ArticlePage from "./pages/ArticlePage";
 import ArticlesListPage from "./pages/ArticlesListPage";
@@ -7,14 +8,20 @@ import NotFoundPage from "./pages/NotFoundPage";
 
 function App(){
     return(
-        <>
-            <AboutPage/>
-            <HomePage/>
-            <ArticlePage/>
-            <ArticlesListPage/>
-            <NotFoundPage/>
-
-        </>
+        <BrowserRouter>
+            <div className="App">
+                <h1>First React App</h1>
+            <div id="page-body">
+            <Routes>
+                <Route path="/" element={<HomePage/>}></Route>
+                <Route path="/articles" element={<ArticlesListPage/>}></Route>
+                <Route path="/article" element={<ArticlePage/>}></Route>
+                <Route path="/about" element={<AboutPage/>}></Route>
+                <Route path="*" element={<NotFoundPage/>}></Route>
+            </Routes>
+            </div>
+            </div>
+        </BrowserRouter>
     );
 }
 
