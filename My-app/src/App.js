d55d7788dca2b3774345cced12349221523deb8f
Sanjay <sanjaykumarbe575@gmail.com>
2024-02-26 14:58:09 +0530

    Duummy Database article-content.js created

--- My-app/src/App.js
+++ My-app/src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
+import "./App.css";
 import {BrowserRouter,Routes,Route} from "react-router-dom";
 import AboutPage from "./pages/AboutPage";
 import ArticlePage from "./pages/ArticlePage";
 import ArticlesListPage from "./pages/ArticlesListPage";
 import HomePage from "./pages/HomePage";
 import NotFoundPage from "./pages/NotFoundPage";
+import Navbar from "./NavBar";
 
 function App(){
     return(
         <BrowserRouter>
             <div className="App">
                 <h1>First React App</h1>
+                <Navbar/>
             <div id="page-body">
             <Routes>
                 <Route path="/" element={<HomePage/>}></Route>
--- My-app/src/pages/HomePage.js
+++ My-app/src/pages/HomePage.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <div>
+      <p>
+      Upon entering, one is greeted by the rich scent of old paper and the soft rustle of pages being turned. Sunlight filters through the stained-glass windows, casting colorful patterns on the well-worn wooden floors. Rows upon rows of bookshelves line the walls, each overflowing with literary treasures waiting to be discovered
+      </p>
+      <p>
+      Upon entering, one is greeted by the rich scent of old paper and the soft rustle of pages being turned. Sunlight filters through the stained-glass windows, casting colorful patterns on the well-worn wooden floors. Rows upon rows of bookshelves line the walls, each overflowing with literary treasures waiting to be discovered
+      </p>
+      <p>
+      Upon entering, one is greeted by the rich scent of old paper and the soft rustle of pages being turned. Sunlight filters through the stained-glass windows, casting colorful patterns on the well-worn wooden floors. Rows upon rows of bookshelves line the walls, each overflowing with literary treasures waiting to be discovered
+      </p>
+      </div>
   );
 }
 
