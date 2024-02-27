97f1def1964f888004ec5f3a7387bd2975e2bc0f
Sanjay <sanjaykumarbe575@gmail.com>
2024-02-27 16:11:42 +0530

    Created Backend

--- My-app/src/pages/ArticlesListPage.js
+++ My-app/src/pages/ArticlesListPage.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import articles from "./articles-content"
-import {Link} from "react-router-dom";
+import articles from './articles-content';
+import ArticleList from '../components/ArticleList';
 
 
 function ArticlesListPage() {
   return (
     <>
-    <h1>List of Articles</h1>
-    {articles.map((article)=>(
-      <Link key={article.name} className="article-list-item" to={`/article/${article.name}`}>
-        <h3>{article.title}</h3>
-        <p>{article.content[0].substring(0,100)}...</p>
-      </Link>
-    ))}
+    <ArticleList articles={articles}/>
     </>
   );
 }
