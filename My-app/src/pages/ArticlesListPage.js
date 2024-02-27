eb451b7d5c050d92037d4ee063c010bb909fd7a0
Sanjay <sanjaykumarbe575@gmail.com>
2024-02-27 15:27:09 +0530

    Successfully added re-usable component ArticleList

--- My-app/src/pages/ArticlesListPage.js
+++ My-app/src/pages/ArticlesListPage.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import articles from "./articles-content"
+import {Link} from "react-router-dom";
+
 
 function ArticlesListPage() {
   return (
-    <div>ArticleslistPage</div>
-  )
+    <>
+    <h1>List of Articles</h1>
+    {articles.map((article)=>(
+      <Link key={article.name} className="article-list-item" to={`/article/${article.name}`}>
+        <h3>{article.title}</h3>
+        <p>{article.content[0].substring(0,100)}...</p>
+      </Link>
+    ))}
+    </>
+  );
 }
 
 export default ArticlesListPage;
